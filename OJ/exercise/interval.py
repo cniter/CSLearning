948adfa0b45e32d500e7420c58f2ddec7fbd8437
cniter <shaunyuan@foxmail.com>
2023-03-01 00:14:55 +0800

    test interval.py

--- OJ/exercise/interval.py
+++ OJ/exercise/interval.py
@@ -48,10 +48,11 @@ def update_interval(rngs, start, end, eps=1E-5):
             res_rngs.append(rng)
             continue
         if rng[0] <= start:
-            res_rngs.append([rng[0], start])
+            if rng[0] != start:
+                res_rngs.append([rng[0], start])
             res_rngs.append([start, end])
             insert_idx = len(res_rngs)-1
-        if end <= rng[1]:
+        if end < rng[1]:
             res_rngs.append([end, rng[1]])
     
     if not res_rngs:
@@ -63,6 +64,10 @@ def update_interval(rngs, start, end, eps=1E-5):
     return res_rngs
 
 def update_interval_test():
+    a = [[1,2],[2,3],[3,4]]
+    for item in [[3.5,3.7], [2.0000001,2.999999]]:
+        a = update_interval(a, item[0], item[1])
+    print(a)
     print(update_interval([[1,2],[2,3],[3,4]], 2.0000001,2.999999))
 
 update_interval_test()
